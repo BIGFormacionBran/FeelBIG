e13ebbbe8f5ea64d390e336bfe3300d0606fdd9b
srfrutos <sergiodfgl@email.com>
2026-04-24 18:23:44 +0200

    update: 24/04/2026 18:23:42,55

--- includes/components/footer.php
+++ includes/components/footer.php
@@ -3,12 +3,5 @@
         <div class="footer-content">
             <p>&copy; <?php echo date('Y'); ?> <strong>Feel BiG</strong> - Todos los derechos reservados.</p>
         </div>
-        
-        <?php 
-        // Reutilizamos la firma corporativa definida en firma.php
-        if(function_exists('renderInfoFooter')) {
-            echo renderInfoFooter(); 
-        }
-        ?>
     </div>
 </footer>
