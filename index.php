56378254f7d9049ac5a6abfaa1d97d1e50d8eb2b
srfrutos <sergiodfgl@email.com>
2026-04-24 15:42:24 +0200

    autocommit: 24/04/2026 15:42:23,62

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
+    
     <link rel="stylesheet" href="assets/css/styles.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
     <title>Feel BiG</title>
