f2aee7d51bac65078b31f3980cd96914be4cfb44
srfrutos <sergiodfgl@email.com>
2026-04-27 14:52:51 +0200

    update: 27/04/2026 14:52:48,59

--- includes/components/home_modules/01_minijuegos.php
+++ includes/components/home_modules/01_minijuegos.php
@@ -1,11 +1,22 @@
 <?php
 $title = "Minijuegos Saludables";
 $viewAllLink = "index.php?page=minijuegos";
-// El link ahora se genera automáticamente en el renderizador usando el ID o un slug
+
 $items = [
-    ["id" => "1", "name" => "Salud Canarias Gaming", "img" => "game1.jpg", "badge" => "Interactivo"],
-    ["id" => "2", "name" => "Reto Nutrición", "img" => "game2.jpg", "badge" => "Interactivo"],
-    ["id" => "3", "name" => "BiG Memory", "img" => "game3.jpg", "badge" => "Memoria"]
+    [
+        "id"    => "1", 
+        "type"  => "minijuego", // Esto define a dónde irá el enlace
+        "name"  => "Salud Canarias Gaming", 
+        "img"   => "game1.jpg", 
+        "badge" => "Interactivo"
+    ],
+    [
+        "id"    => "2", 
+        "type"  => "minijuego", 
+        "name"  => "Reto Nutrición", 
+        "img"   => "game2.jpg", 
+        "badge" => "Interactivo"
+    ]
 ];
 
 include __DIR__ . '/../carousel_template.php';
--- includes/utils/card_render_util.php
+++ includes/utils/card_render_util.php
@@ -1,26 +1,27 @@
 <?php
 function render_card_item_util($item) {
-    // Generación automática del link según tu petición
-    $detailLink = "index.php?page=minijuego_detalle&id=" . $item['id'];
+    // Generación automática: index.php?page=item&type=minijuego&id=1
+    // Usamos 'item' como página global y pasamos los parámetros necesarios
+    $detailLink = "index.php?page=item&type=" . ($item['type'] ?? 'default') . "&id=" . $item['id'];
     ?>
-        <div class="card-item">
-            <div class="thumb">
-                <img src="assets/img/<?php echo $item['img']; ?>" alt="<?php echo $item['name']; ?>" class="thumb-img">
-                <?php if (isset($item['badge'])): ?>
-                    <span class="badge"><?php echo $item['badge']; ?></span>
+    <div class="card-item">
+        <div class="thumb">
+            <img src="assets/img/<?php echo $item['img']; ?>" alt="<?php echo $item['name']; ?>" class="thumb-img">
+            <?php if (isset($item['badge'])): ?>
+                <span class="badge"><?php echo $item['badge']; ?></span>
+            <?php endif; ?>
+        </div>
+        <div class="card-content">
+            <div class="card-info">
+                <h4><?php echo $item['name']; ?></h4>
+                <?php if(isset($item['fecha'])): ?>
+                    <p class="card-date"><strong>Fecha:</strong> <?php echo $item['fecha']; ?></p>
                 <?php endif; ?>
             </div>
-            <div class="card-content">
-                <div class="card-info">
-                    <h4><?php echo $item['name']; ?></h4>
-                    <?php if(isset($item['fecha'])): ?>
-                        <p class="card-date"><strong>Fecha:</strong> <?php echo $item['fecha']; ?></p>
-                    <?php endif; ?>
-                </div>
-                <a href="<?php echo $detailLink; ?>">
-                    <div class="btn-primario btn-card">MÁS INFORMACIÓN</div>
-                </a>
-            </div>
+            <a href="<?php echo $detailLink; ?>">
+                <div class="btn-primario btn-card">MÁS INFORMACIÓN</div>
+            </a>
         </div>
+    </div>
     <?php
 }
