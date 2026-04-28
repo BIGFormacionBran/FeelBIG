5ee826bac5abbae9fd4cc30a672e82382c59af1b
srfrutos <sergiodfgl@email.com>
2026-04-28 14:40:12 +0200

    update: 28/04/2026 14:40:12,49

--- includes/pages/auth_view.php
+++ includes/pages/auth_view.php
@@ -44,7 +44,7 @@ if ($error_code === '1') {
                     <input type="password" name="password" id="passInput" placeholder="Contraseña" required minlength="6">
                     <button type="button" class="btn-ojo" id="toggleBtn" aria-label="Mostrar contraseña">
                         <svg class="icon-open" viewBox="0 0 24 24" fill="currentColor">
-                            <path d="M12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6zm0-4c7 0 11 8 11 8s-4 8-11 8-11-8-11-8 4-8 11-8zm0 14a6 6 0 1 1 0-12 6 6 0 0 1 0 12z"/>
+                            <path d="M12 5c-7 0-10 7-10 7s3 7 10 7 10-7 10-7-3-7-10-7zm0 12c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/>
                         </svg>
                         
                         <svg class="icon-closed" viewBox="0 0 24 24" fill="currentColor" style="display:none;">
