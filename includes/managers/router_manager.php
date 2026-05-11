49933afa36355e7f1ffa986af3b326823547f013
srfrutos <sergiodfgl@email.com>
2026-05-11 13:46:43 +0200

    update: 11/05/2026 13:46:42,19

--- includes/managers/router_manager.php
+++ includes/managers/router_manager.php
@@ -4,6 +4,8 @@ require_once __DIR__ . '/content_manager.php';
 function get_page_config_manager($page) {
     $baseDir = __DIR__ . '/../../includes/pages/';
 
+    global $routeParts;
+
     if ($page === 'error') {
         $errorCode = $GLOBALS['routeParts'][1] ?? '404';
         return [
