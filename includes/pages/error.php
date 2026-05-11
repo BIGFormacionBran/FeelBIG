d5061b7a413174f25ced7a433fb0250b240fb8e2
srfrutos <sergiodfgl@email.com>
2026-05-11 11:27:21 +0200

    update: 11/05/2026 11:27:20,64

--- includes/pages/error.php
+++ includes/pages/error.php
@@ -3,6 +3,10 @@ $errorCode = $pageConfig['error_code'] ?? '404';
 if(is_numeric($errorCode)) http_response_code($errorCode);
 ?>
 <div class="error-container">
-    <div class="titulo-acceso">Error <?php echo $errorCode; ?></div>
-    <a href="/home" class="btn-primario">Volver al Inicio</a>
+    <div class="error-header">
+        <div class="titulo-acceso">Error <?php echo $errorCode; ?></div>
+    </div>
+    <div class="error-footer">
+        <a href="/home" class="btn-primario">Volver al Inicio</a>
+    </div>    
 </div>
