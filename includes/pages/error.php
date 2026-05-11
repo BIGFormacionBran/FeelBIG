f522013afb539c6ba2c45c08116cd99192de53a6
srfrutos <sergiodfgl@email.com>
2026-05-11 11:32:15 +0200

    update: 11/05/2026 11:32:14,37

--- includes/pages/error.php
+++ includes/pages/error.php
@@ -4,7 +4,7 @@ if(is_numeric($errorCode)) http_response_code($errorCode);
 ?>
 <div class="error-container">
     <div class="error-header">
-        <div class="titulo-acceso">Error <?php echo $errorCode; ?></div>
+        <div class="titulo-error">Error <?php echo $errorCode; ?></div>
     </div>
     <div class="error-footer">
         <a href="/home" class="btn-primario">Volver al Inicio</a>
