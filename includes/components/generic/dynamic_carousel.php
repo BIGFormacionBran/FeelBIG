d9376bae74f344c6b7e7d10ffed06be15954efdc
srfrutos <sergiodfgl@email.com>
2026-05-12 14:59:55 +0200

    update: 12/05/2026 14:59:53,86

--- includes/components/generic/dynamic_carousel.php
+++ includes/components/generic/dynamic_carousel.php
@@ -2,27 +2,29 @@
 if (!empty($carouselItems)):
     $carouselID = 'fb_gen_cur_' . uniqid(); 
 ?>
-<div class="home-module-wrapper feelbig-swiper-section" id="<?php echo $carouselID; ?>">
-    <div class="module-header">
-        <h2><?php echo htmlspecialchars($carouselTitle); ?></h2>
-    </div>
-
-    <div class="carousel-flex-layout">
-        <div class="swiper-button-prev btn-nav-feelbig"></div>
-
-        <div class="swiper swiper-feelbig-generic">
-            <div class="swiper-wrapper">
-                <?php foreach ($carouselItems as $item): ?>
-                    <div class="swiper-slide">
-                        <?php render_card_item_column($item); ?>
-                    </div>
-                <?php endforeach; ?>
-            </div>
+<div class="container-page">
+    <div class="home-module-wrapper feelbig-swiper-section" id="<?php echo $carouselID; ?>">
+        <div class="module-header">
+            <h2><?php echo htmlspecialchars($carouselTitle); ?></h2>
         </div>
 
-        <div class="swiper-button-next btn-nav-feelbig"></div>
-    </div>
+        <div class="carousel-flex-layout">
+            <div class="swiper-button-prev btn-nav-feelbig"></div>
 
-    <div class="swiper-pagination-custom"></div>
+            <div class="swiper swiper-feelbig-generic">
+                <div class="swiper-wrapper">
+                    <?php foreach ($carouselItems as $item): ?>
+                        <div class="swiper-slide">
+                            <?php render_card_item_column($item); ?>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+
+            <div class="swiper-button-next btn-nav-feelbig"></div>
+        </div>
+
+        <div class="swiper-pagination-custom"></div>
+    </div>
 </div>
 <?php endif; ?>
--- includes/pages/home.php
+++ includes/pages/home.php
@@ -3,11 +3,6 @@
 require_once 'includes/utils/render_util.php';
 ?>
 
-<div class="hero-feelbig">
-    <h1>Bienvenido a Feel BiG</h1>
-    <p>Soluciones de bienestar para empresas y familias.</p>
-</div>
-
 <div class="home-dynamic-container">
     <?php 
         render_auto_components_util('includes/components/home_modules'); 
