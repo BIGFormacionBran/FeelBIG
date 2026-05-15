4cf5700243ce68a2d8fdc8d2e177b55d3955e05b
srfrutos <sergiodfgl@email.com>
2026-05-15 14:28:38 +0200

    update: 15/05/2026 14:28:37,28

--- includes/managers/MainManager.php
+++ includes/managers/MainManager.php
@@ -20,34 +20,41 @@ class MainManager {
     public function updateProfile($id, $name, $email, $pass) { return $this->userManager->updateProfile($id, $name, $email, $pass); }
 
     public function getBreadcrumbs($currentPage, $routeParts) {
-        LoggerUtil::info("MainManager: Generating breadcrumbs for $currentPage");
+        LoggerUtil::info("CRUMB_LOG: Input -> Page: [$currentPage] | RouteParts: " . json_encode($routeParts));
         if (in_array($currentPage, ['home', 'login', 'register', 'configuracion', 'error'])) return null;
         
         if ($currentPage === 'admin' && isset($routeParts[1])) {
             $breadcrumbs = [['title' => 'Admin', 'link' => '/admin']];
             $breadcrumbs[] = ['title' => ucwords(str_replace(['-', '_'], ' ', urldecode($routeParts[1]))), 'link' => null];
-        }else {
+        } else {
             $breadcrumbs = [['title' => 'Home', 'link' => '/home']];
         }
         
         $currentSlug = ($currentPage === 'IndividualView') ? ($routeParts[0] ?? null) : $currentPage;
+        LoggerUtil::info("CRUMB_LOG: Calculated Slug to search: [$currentSlug]");
         if ($currentSlug) {
             $categoryData = $this->contentDao->getCategoryBySlug($currentSlug);
             if ($categoryData) {
+                LoggerUtil::info("CRUMB_LOG: Category found: " . $categoryData['nombre']);
                 if (!empty($categoryData['id_padre'])) {
                     $parent = $this->contentDao->getCategoryById($categoryData['id_padre']);
                     if ($parent) {
+                        LoggerUtil::info("CRUMB_LOG: Parent category found: " . $parent['nombre']);
                         $breadcrumbs[] = ['title' => $parent['nombre'], 'link' => '/' . strtolower(str_replace(' ', '-', $parent['nombre']))];
                     }
                 }
                 $isIndividual = ($currentPage === 'IndividualView');
+                LoggerUtil::info("CRUMB_LOG: Is Individual? " . ($isIndividual ? 'YES' : 'NO'));
                 $breadcrumbs[] = ['title' => $categoryData['nombre'], 'link' => $isIndividual ? '/' . $currentSlug : null];
                 
                 if ($isIndividual) {
-                    $breadcrumbs[] = ['title' => ucwords(str_replace(['-', '_'], ' ', urldecode($routeParts[1]))), 'link' => null];
+                    $itemName = isset($routeParts[1]) ? ucwords(str_replace(['-', '_'], ' ', urldecode($routeParts[1]))) : 'Desconocido';
+                    $breadcrumbs[] = ['title' => $itemName, 'link' => null];
+                    LoggerUtil::info("CRUMB_LOG: Adding individual item: [$itemName]");
                 }
             }
         }
+        LoggerUtil::info("CRUMB_LOG: Final Breadcrumbs count: " . count($breadcrumbs));
         return $breadcrumbs;
     }
 }
